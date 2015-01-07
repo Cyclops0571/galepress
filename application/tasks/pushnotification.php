3f5e310b51c650c1891632eb7949dedca847f7db
uet laptop <emre.terzi@gurus.com.tr>
2015-01-08 01:49:32 +0200

    Fixed #2 - ios ve android icin komut ismeniden cagirilabilen iki ayri fonksiyon eklendi

--- application/tasks/pushnotification.php
+++ application/tasks/pushnotification.php
@@ -2,10 +2,227 @@
 
 class PushNotification_Task {
 
-    public function run()
-    {
-    	//https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Introduction.html
-    	//https://developer.apple.com/library/ios/technotes/tn2265/_index.html
+	public function run()
+	{
+		//https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Introduction.html
+		//https://developer.apple.com/library/ios/technotes/tn2265/_index.html
+		try
+		{
+			$pn = DB::table('Customer AS c')
+					->join('Application AS a', function($join)
+					{
+						$join->on('a.CustomerID', '=', 'c.CustomerID');
+						$join->on('a.StatusID', '=', DB::raw(eStatus::Active));
+					})
+					->join('PushNotification AS p', function($join)
+					{
+						$join->on('p.CustomerID', '=', 'c.CustomerID');
+						$join->on('p.ApplicationID', '=', 'a.ApplicationID');
+						$join->on('p.StatusID', '=', DB::raw(eStatus::Active));
+					})
+					->join('PushNotificationDevice AS d', function($join)
+					{
+						$join->on('d.PushNotificationID', '=', 'p.PushNotificationID');
+						$join->on('d.Sent', '=', DB::raw(0));
+						$join->on('d.ErrorCount', '<', DB::raw(2));
+						$join->on('d.StatusID', '=', DB::raw(eStatus::Active));
+					})
+					->where('c.StatusID', '=', eStatus::Active)
+					->order_by('p.PushNotificationID', 'DESC')
+					->order_by('d.PushNotificationDeviceID', 'DESC')
+					->take(1000)
+					->get(array('c.CustomerID', 'a.ApplicationID', 'a.CkPem', 'p.PushNotificationID', 'p.NotificationText', 'd.PushNotificationDeviceID', 'd.DeviceToken', 'd.DeviceType'));
+				
+			foreach($pn as $n)
+			{
+				try
+				{
+					$result = false;
+
+					//ios
+					if($n->DeviceType === 'ios')
+					{
+						$cert = path('public').'files/customer_'.$n->CustomerID.'/application_'.$n->ApplicationID.'/'.$n->CkPem;
+
+						$result = $this->ios_ckpem($cert, $n->NotificationText, $n->DeviceToken);
+					}
+					//android
+					elseif($n->DeviceType === 'android')
+					{
+						$result = $this->android($n->ApplicationID, $n->NotificationText, $n->DeviceToken);
+					}
+
+					if ($result)
+					{
+						$c = PushNotificationDevice::find((int)$n->PushNotificationDeviceID);
+						$c->Sent = 1;
+						$c->save();
+					}
+					else
+					{
+						//throw new Exception('Message not delivered!');
+						$c = PushNotificationDevice::find((int)$n->PushNotificationDeviceID);
+						$c->ErrorCount = (int)$c->ErrorCount + 1;
+						$c->LastErrorDetail = 'Message not delivered!';
+						$c->save();
+					}
+				}
+				catch (Exception $e)
+				{
+					$c = PushNotificationDevice::find((int)$n->PushNotificationDeviceID);
+					$c->ErrorCount = (int)$c->ErrorCount + 1;
+					$c->LastErrorDetail = $e->getMessage();
+					$c->save();
+				}
+			}
+		}
+		catch (Exception $e)
+		{
+			$toEmail = Config::get('custom.admin_email');
+			$subject = __('common.task_subject');
+			$msg = __('common.task_message', array(
+					'task' => '`PushNotification`',
+					'detail' => $e->getMessage()
+					)
+				);
+			
+			Log::info($msg);
+			
+			Bundle::start('messages');
+			
+			Message::send(function($m) use($toEmail, $subject, $msg)
+			{
+				$m->from(Config::get('custom.mail_email'), Config::get('custom.mail_displayname'));
+				$m->to($toEmail);
+				$m->subject($subject);
+				$m->body($msg);
+			});
+		}
+	}
+
+	public function ios($applicationID, $message, $deviceToken)
+	{
+		$app = DB::table('Application')
+				->where('ApplicationID', '=', (int)$applicationID)
+				->first();
+		if(!$app) {
+			throw new Exception('Application not found!');
+		}
+		$cert = path('public').'files/customer_'.$app->CustomerID.'/application_'.$app->ApplicationID.'/'.$app->CkPem;
+		return $this->ios_ckpem($cert, $message, $deviceToken);
+	}
+
+	public function ios_ckpem($cert, $message, $deviceToken)
+	{
+		$success = false;
+
+		// Put your private key's passphrase here:
+		$passphrase = Config::get('custom.passphrase');
+		
+		//Log::info('crt:'.$cert.' devToken:'.$deviceToken.' passphrase:'.$passphrase.' message:'.$message);
+		////////////////////////////////////////////////////////////////////////////////
+		
+		$ctx = stream_context_create();
+		stream_context_set_option($ctx, 'ssl', 'local_cert', $cert);
+		stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+		
+		// Open a connection to the APNS server
+		$fp = stream_socket_client(
+			'ssl://gateway.push.apple.com:2195', $err,
+			$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+			
+		if ($fp)
+		{
+			// Create the payload body
+			$body['aps'] = array(
+				'alert' => $message,
+				'sound' => 'default'
+				);
+			
+			// Encode the payload as JSON
+			$payload = json_encode($body);
+			
+			// Build the binary notification
+			$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+			
+			// Send it to the server
+			$result = fwrite($fp, $msg, strlen($msg));
+				
+			//Log::info($result);
+
+			if ($result)
+			{
+				$success = true;
+			}
+			// Close the connection to the server
+			fclose($fp);	
+		}
+		else
+		{
+			//throw new Exception("Failed to connect: $err $errstr" . PHP_EOL);
+		}
+		return $success;
+	}
+
+	public function android($applicationID, $message, $deviceToken)
+	{
+		$success = false;
+		//$googleAPIKey = 'AIzaSyCj2v2727lBWLeXbgM_Hw_VEQgzjDgb8KY';
+		$googleAPIKey = Config::get('custom.google_api_key');
+
+		$data = array(
+			'headers' => array(
+				'Authorization: key='.$googleAPIKey,
+				'Content-Type: application/json'
+			),
+			'fields' => array(
+				'registration_ids' => array(
+					$deviceToken
+				),
+				'data' => array(
+					"message" => $message
+				)
+			)
+		);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $data['headers']);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Disabling SSL Certificate support temporarly
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data['fields']));
+		$result = curl_exec($ch);
+		if ($result === false) {
+			//die('Curl failed: ' . curl_error($ch));
+			throw new Exception('Curl failed: '.curl_error($ch));
+		}
+		curl_close($ch);
+		$json = json_decode($result, true);
+		if($json['success'] === 1)
+		{
+			$success = true;
+		}
+		return $success;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public function run_backup()
+	{
+		//https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Introduction.html
+		//https://developer.apple.com/library/ios/technotes/tn2265/_index.html
 		try
 		{
 			$pn = DB::table('Customer AS c')
@@ -214,5 +431,5 @@ class PushNotification_Task {
 				$m->body($msg);
 			});
 		}
-    }
+	}
 }
