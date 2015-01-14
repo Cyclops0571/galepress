63baad7683d87256f9fd5d770adcce5892f7c5e1
uet laptop <emre.terzi@gurus.com.tr>
2015-01-14 22:51:48 +0200

    iOS & android filter added

--- public/files/report.sql/302.sql
+++ public/files/report.sql/302.sql
@@ -1,34 +1,31 @@
 SELECT `Device`, COUNT(*) AS `DownloadCount` 
-FROM (
+FROM ( 
 	SELECT 
-		`DeviceType`,
+		cu.`CustomerID`, cu.`CustomerNo`, cu.`CustomerName`, 
+		ap.`ApplicationID`, ap.`Name` AS `ApplicationName`, ap.`ExpirationDate`, ap.`ApplicationStatusID`, IFNULL(ap.`Blocked`, 0) AS `ApplicationBlocked`, 
+		cn.`ContentID`, cn.`Name` AS `ContentName`, IFNULL(cn.`Approval`, 0) AS `ContentApproval`, IFNULL(cn.`Blocked`, 0) AS `ContentBlocked`, 
+		rq.`DeviceType`,
 		(
 		CASE  
-			WHEN INSTR(`DeviceType`, 'iPhone') > 0 THEN 'iOS' 
-			WHEN INSTR(`DeviceType`, 'iPad') > 0 THEN 'iOS' 
-			WHEN INSTR(`DeviceType`, 'iPod') > 0 THEN 'iOS' 
-			WHEN INSTR(`DeviceType`, 'BlackBerry') > 0 THEN 'BlackBerry' 
-			WHEN INSTR(`DeviceType`, 'Android') > 0 THEN 'Android' 
-			WHEN INSTR(`DeviceType`, 'Windows') > 0 THEN 'Windows' 
-			WHEN INSTR(`DeviceType`, 'Linux') > 0 THEN 'Linux' 
+			WHEN INSTR(rq.`DeviceType`, 'iPhone') > 0 THEN 'iOS' 
+			WHEN INSTR(rq.`DeviceType`, 'iPad') > 0 THEN 'iOS' 
+			WHEN INSTR(rq.`DeviceType`, 'iPod') > 0 THEN 'iOS' 
+			WHEN INSTR(rq.`DeviceType`, 'BlackBerry') > 0 THEN 'BlackBerry' 
+			WHEN INSTR(rq.`DeviceType`, 'Android') > 0 THEN 'Android' 
+			WHEN INSTR(rq.`DeviceType`, 'Windows') > 0 THEN 'Windows' 
+			WHEN INSTR(rq.`DeviceType`, 'Linux') > 0 THEN 'Linux' 
 			ELSE 'Other' 
 		END
 		) AS `Device` 
-	FROM ( 
-		SELECT 
-			cu.`CustomerID`, cu.`CustomerNo`, cu.`CustomerName`, 
- 			ap.`ApplicationID`, ap.`Name` AS `ApplicationName`, ap.`ExpirationDate`, ap.`ApplicationStatusID`, IFNULL(ap.`Blocked`, 0) AS `ApplicationBlocked`, 
- 			cn.`ContentID`, cn.`Name` AS `ContentName`, IFNULL(cn.`Approval`, 0) AS `ContentApproval`, IFNULL(cn.`Blocked`, 0) AS `ContentBlocked`, 
-			rq.`DeviceType`
-		FROM `Customer` cu 
-			INNER JOIN `Application` ap ON ap.`CustomerID`=cu.`CustomerID` AND ap.`StatusID`=1 
-			INNER JOIN `Content` cn ON cn.`ApplicationID`=ap.`ApplicationID` AND cn.`StatusID`=1
-			INNER JOIN `Request` rq ON rq.`ContentID`=cn.`ContentID` AND rq.`RequestTypeID`=1001 AND rq.`RequestDate` BETWEEN '{SD}' AND '{ED}' AND rq.`Percentage`=100
-		WHERE
-			cu.`CustomerID`=COALESCE({CUSTOMERID}, cu.`CustomerID`) AND 
-			ap.`ApplicationID`=COALESCE({APPLICATIONID}, ap.`ApplicationID`) AND 
-			cn.`ContentID`=COALESCE({CONTENTID}, cn.`ContentID`) AND 
-			cu.`StatusID`=1
-	) t
-) y 
+	FROM `Customer` cu 
+		INNER JOIN `Application` ap ON ap.`CustomerID`=cu.`CustomerID` AND ap.`StatusID`=1 
+		INNER JOIN `Content` cn ON cn.`ApplicationID`=ap.`ApplicationID` AND cn.`StatusID`=1
+		INNER JOIN `Request` rq ON rq.`ContentID`=cn.`ContentID` AND rq.`RequestTypeID`=1001 AND rq.`RequestDate` BETWEEN '{SD}' AND '{ED}' AND rq.`Percentage`=100
+	WHERE
+		cu.`CustomerID`=COALESCE({CUSTOMERID}, cu.`CustomerID`) AND 
+		ap.`ApplicationID`=COALESCE({APPLICATIONID}, ap.`ApplicationID`) AND 
+		cn.`ContentID`=COALESCE({CONTENTID}, cn.`ContentID`) AND 
+		cu.`StatusID`=1
+) t
+WHERE `Device` IN ('iOS', 'Android')
 GROUP BY `Device`
