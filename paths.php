37513b3eda4c56130833008692f3a9b0543ee9b8
Cyclops0571 <srdsaygili@gmail.com>
2015-03-13 18:55:35 +0200

    vagrant pathi de localhost environmenta alindi.

--- paths.php
+++ paths.php
@@ -23,7 +23,7 @@
 
 $environments = array(
 
-	'local' => array('http://localhost*', '*.dev'),
+	'local' => array('http://localhost*', '*.dev', '*vagrant*'),
 	'test' => array('*.galetest.com*'),
 	'live' => array('*.galepress.com*')
 
