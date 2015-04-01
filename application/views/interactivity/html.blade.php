eeff5e08f09d45b670831d34b128695ae53e41b8
hknsrr <bilgi@hakansarier.com>
2015-04-01 17:58:53 +0300

    interactivity js dosyalari icin app version parametresi eklendi.

--- application/views/interactivity/html.blade.php
+++ application/views/interactivity/html.blade.php
@@ -37,56 +37,56 @@
 <![endif]-->
 
 @include('js.language')
-<script src="/js/modernizr.js" type="text/javascript"></script><!-- IE Fix for HTML5 Tags -->
+<script src="/js/modernizr.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script><!-- IE Fix for HTML5 Tags -->
 
 <!--
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js?v=<?php echo APP_VER; ?>"></script>
 -->
 
-<script src="/js/vendor/jquery.min.js" type="text/javascript"></script>
-<script src="/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="/js/jquery.base64.decode.js" type="text/javascript"></script>
-<script src="/js/gurus.common.js" type="text/javascript"></script>
-<script src="/js/gurus.string.js" type="text/javascript"></script>
-<script src="/js/gurus.date.js" type="text/javascript"></script>
-<script src="/js/vendor/jquery-ui.js"></script>
-<script src="/uploadify/jquery.uploadify-3.1.min.js" type="text/javascript"></script>
-<script src="/js/fullscreen.js" type="text/javascript"></script>
-<script src="/js/SCF.ui.js" type="text/javascript"></script>
-<script src="/js/vendor/chosen.jquery.js" type="text/javascript"></script>
-<script src="/js/vendor/jquery.placeholder.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/Equalizer.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/appreciate.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/commutator.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/datepicker.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/pagination.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/scrollbox.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/slideshow.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/tabbox.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/starbar.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/checkbox.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/radio.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/player.js" type="text/javascript"></script>
-<script src="/js/SCF.ui/currentlyPlaying.js" type="text/javascript"></script>
-<script src="/js/jquery.cookie.js"></script>
-<script src="/js/zoom_assets/jquery.smoothZoom.js?v=10"></script>
-<script src="/js/jquery.gselectable.js?v=10" type="text/javascript"></script>
-<script src="/js/jquery.component.js?v=10" type="text/javascript"></script>
+<script src="/js/vendor/jquery.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/bootstrap.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/jquery.base64.decode.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/gurus.common.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/gurus.string.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/gurus.date.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/vendor/jquery-ui.js?v=<?php echo APP_VER; ?>"></script>
+<script src="/uploadify/jquery.uploadify-3.1.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/fullscreen.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/vendor/chosen.jquery.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/vendor/jquery.placeholder.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/Equalizer.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/appreciate.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/commutator.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/datepicker.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/pagination.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/scrollbox.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/slideshow.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/tabbox.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/starbar.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/checkbox.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/radio.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/player.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/SCF.ui/currentlyPlaying.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/jquery.cookie.js?v=<?php echo APP_VER; ?>"></script>
+<script src="/js/zoom_assets/jquery.smoothZoom.js?v=<?php echo APP_VER; ?>"></script>
+<script src="/js/jquery.gselectable.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/jquery.component.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
 <!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js?v=<?php echo APP_VER; ?>"></script>
 <![endif]-->
 </head>
 <body>
 <input type="hidden" id="currentlanguage" value="{{ Session::get('language') }}" />
 @_yield('body-content')
-<script src="/js/jquery.collapse.js" type="text/javascript"></script>
-<script src="/js/jquery.easytabs.js" type="text/javascript"></script>
-<script src="/js/redactor.min.js" type="text/javascript"></script>
-<script src="/js/jquery.colorPicker.js" type="text/javascript"></script>
-<script src="/js/respond.min.js" type="text/javascript"></script>
-<script src="/bundles/jupload/js/jquery.iframe-transport.js" type="text/javascript"></script>
-<script src="/bundles/jupload/js/jquery.fileupload.js" type="text/javascript"></script>
+<script src="/js/jquery.collapse.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/jquery.easytabs.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/redactor.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/jquery.colorPicker.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/respond.min.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/bundles/jupload/js/jquery.iframe-transport.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/bundles/jupload/js/jquery.fileupload.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
 
 <?php
 $components = DB::table('Component')
@@ -99,8 +99,8 @@ $components = DB::table('Component')
 <script id="prop-{{ $component->Class }}" type="text/html">{{ View::make('interactivity.components.'.$component->Class.'.property', array('ComponentID' => $component->ComponentID, 'PageComponentID' => 0, 'Process' => 'new', 'PageCount' => count($pages))) }}</script>
 @endforeach
 
-<script src="/js/gurus.projectcore.js?v=9" type="text/javascript"></script>
-<script src="/js/session-check.js" type="text/javascript"></script>
-<script src="/js/lib.interactivity.js?v=9" type="text/javascript"></script>
+<script src="/js/gurus.projectcore.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/session-check.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
+<script src="/js/lib.interactivity.js?v=<?php echo APP_VER; ?>" type="text/javascript"></script>
 </body>
 </html>
