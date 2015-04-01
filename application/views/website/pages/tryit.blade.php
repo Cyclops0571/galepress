94ff944b491e676d33d39fe964046cedc4e87c03
hknsrr <bilgi@hakansarier.com>
2015-04-01 18:57:30 +0300

    try it sayfasi reklam kodu guncellendi.

--- application/views/website/pages/tryit.blade.php
+++ application/views/website/pages/tryit.blade.php
@@ -273,22 +273,29 @@
             },750);
             alert("{{__('website.tryit_form_message_mail')}}");
 
-            var _fbq = window._fbq || (window._fbq = []);
-            if (!_fbq.loaded) {
-              var fbds = document.createElement('script');
-              fbds.async = true;
-              fbds.src = '//connect.facebook.net/en_US/fbds.js';
-              var s = document.getElementsByTagName('script')[0];
-              s.parentNode.insertBefore(fbds, s);
-              _fbq.loaded = true;
-            }
 
-            window._fbq = window._fbq || [];
-            window._fbq.push(['track', '6022106543310', {'value':'0.00','currency':'USD'}]);
+            /*Google Code for Website Conversion Galepress Conversion*/
+            /* <![CDATA[ */
+            var google_conversion_id = 980149592;
+            var google_conversion_language = "en";
+            var google_conversion_format = "3";
+            var google_conversion_color = "ffffff";
+            var google_conversion_label = "vsCnCMHg-VoQ2Mqv0wM";
+            var google_conversion_value = 50.00;
+            var google_conversion_currency = "TRY";
+            var google_remarketing_only = false;
+            /* ]]> */
 
           }
         });
       };
     });
     </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <noscript>
+    <div style="display:inline;">
+      <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/980149592/?value=50.00&amp;currency_code=TRY&amp;label=vsCnCMHg-VoQ2Mqv0wM&amp;guid=ON&amp;script=0"/>
+    </div>
+    </noscript>
+
 @endsection
