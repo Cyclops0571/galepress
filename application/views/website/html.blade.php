fd4cd89a635684447e951101d15021b5636f14bd
hknsrr <bilgi@hakansarier.com>
2015-04-17 11:30:38 +0300

    dil ile alakali kucuk bir duzeltme ve app version arttirildi ve main.js ye eklendi.

--- application/views/website/html.blade.php
+++ application/views/website/html.blade.php
@@ -69,7 +69,7 @@
     <script src="/website/scripts/vendor/bootstrap-hover-dropdown.min.js"></script>
     <script src="/website/scripts/jquery.gmap.min.js"></script>
     <script src="/website/scripts/circle_diagram.js"></script>
-    <script src="/website/scripts/main.js"></script>
+    <script src="/website/scripts/main.js?v=<?php echo APP_VER; ?>"></script>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- application/views/website/pages/blog-iframe-news.blade.php
+++ application/views/website/pages/blog-iframe-news.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
           <div class="row">
             <div class="col-md-12" id="blog-frame-news">
-              <a href="http://www.galepress.com/tr/blog" class="btn btn-primary btn-xs"><i class="fa fa-rss"></i><span style="margin-left:10px;">Blog</span></a>
+              <a href="http://www.galepress.com/{{ Session::get('language') }}/blog" class="btn btn-primary btn-xs"><i class="fa fa-rss"></i><span style="margin-left:10px;">Blog</span></a>
               <iframe src="http://www.galepress.com/blog/?page_id=5894" id="blogIframeNews" scrolling="no" frameborder="0" style="width:100% !important; height:100% !important; overflow:hidden; margin-top:19px;"></iframe>
             </div>
           </div>
