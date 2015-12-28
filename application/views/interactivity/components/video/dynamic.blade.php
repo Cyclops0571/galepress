02466addf770b501b3af1e3f723042cd1c484430
Cyclops0571 <srdsaygili@gmail.com>
2015-12-28 17:17:57 +0200

    Video componentine poster eklenmemiş ise componentin içinde index.htmle request atıyor olmadığı için hata yiyor

--- application/views/interactivity/components/video/dynamic.blade.php
+++ application/views/interactivity/components/video/dynamic.blade.php
@@ -1,33 +1,32 @@
 <?php
-$vFile = path('public').$filename;
-if(File::exists($vFile) && is_file($vFile)) {
-	$fname = File::name($vFile);
-	$fext = File::extension($vFile);
-	$vFile = $fname.'.'.$fext;
-}
-else {
-	$vFile = '';
-}
-
-$vPosterImageFile = path('public').$posterimagename;
-if(File::exists($vPosterImageFile) && is_file($vPosterImageFile)) {
-	$fname = File::name($vPosterImageFile);
-	$fext = File::extension($vPosterImageFile);
-	$vPosterImageFile = $fname.'.'.$fext;
-}
-else {
-	$vPosterImageFile = '';
+$vFile = path('public') . $filename;
+if (File::exists($vFile) && is_file($vFile)) {
+    $fname = File::name($vFile);
+    $fext = File::extension($vFile);
+    $vFile = $fname . '.' . $fext;
+} else {
+    $vFile = '';
 }
 
-if(!$preview)
-{
-	$vFile = $baseDirectory.'comp_'.$id.'/'.$vFile;
-	$vPosterImageFile = $baseDirectory.'comp_'.$id.'/'.$vPosterImageFile;
+$vPosterImageFile = path('public') . $posterimagename;
+if (File::exists($vPosterImageFile) && is_file($vPosterImageFile)) {
+    $fname = File::name($vPosterImageFile);
+    $fext = File::extension($vPosterImageFile);
+    $vPosterImageFile = $fname . '.' . $fext;
+} else {
+    $vPosterImageFile = '';
 }
-else
-{
-	$vFile = '/'.$filename;
-	$vPosterImageFile = '/'.$posterimagename;
+
+if (!$preview) {
+    $vFile = $baseDirectory . 'comp_' . $id . '/' . $vFile;
+    if (!empty($vPosterImageFile)) {
+        $vPosterImageFile = $baseDirectory . 'comp_' . $id . '/' . $vPosterImageFile;
+    }
+} else {
+    $vFile = '/' . $filename;
+    if (!empty($vPosterImageFile)) {
+        $vPosterImageFile = '/' . $posterimagename;
+    }
 }
 
 /*
@@ -41,94 +40,105 @@ else {
 }
 */
 ?>
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1">
-	<script src="{{ $baseDirectory }}comp_{{ $id }}/js/jquery.js"></script>
-	<title>GalePress</title>
-	<link href="{{ $baseDirectory }}comp_{{ $id }}/css/video-js.css" rel="stylesheet" type="text/css">
-	<script src="{{ $baseDirectory }}comp_{{ $id }}/js/video.js" type="text/javascript"></script>
-	<style>
-	*{
-		overflow:hidden !important;
-	}
-	body{
-		margin: 0 !important;
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1">
+    <script src="{{ $baseDirectory }}comp_{{ $id }}/js/jquery.js"></script>
+    <title>GalePress</title>
+    <link href="{{ $baseDirectory }}comp_{{ $id }}/css/video-js.css" rel="stylesheet" type="text/css">
+    <script src="{{ $baseDirectory }}comp_{{ $id }}/js/video.js" type="text/javascript"></script>
+    <style>
+        * {
+            overflow: hidden !important;
+        }
 
-	}
-	.video-js{
-		position: fixed !important;
-		width,height: 100% !important;
-	}
-	.vjs-control-bar{
-		position: fixed !important;
-	}
-	/*Video JS Css*/
-    .vjs-default-skin .vjs-big-play-button {
-      left: 50%;
-      margin-top: -22px;
-      margin-left: -22px;
-      top: 50%;
-      font-size: 2em;
-      width: 2em;
-      height: 2em;
-      -webkit-border-radius: 50%;
-      -moz-border-radius: 50%;
-      border-radius: 50%;
-    }
-    .vjs-default-skin .vjs-big-play-button:before {
-      line-height: 1.5em;
-    }
+        body {
+            margin: 0 !important;
 
-	</style>
-	<script type="text/javascript">
-		$(document).ready(function($)
-		{
-			if({{$modal}}==1){
-				$( "*" ).css('opacity','0');
-				$( "html" ).css('background','black');
-				$( "html" ).css('opacity','1');
-			}
+        }
 
-			if({{$videodelay}}!=0){
-				setTimeout(function() {			
-		      		$("video")[0].play();
-				}, {{$videodelay}});
-			}
-		});
+        .video-js {
+            position: fixed !important;
+            width,
+            height: 100% !important;
+        }
 
-		function startVideo () {
-			var o = videojs('example_video_1');
-			if(o.paused()) {
-				o.play();
-			}
-			else {
-				o.pause();
-			}
-		}
-	</script>
+        .vjs-control-bar {
+            position: fixed !important;
+        }
+
+        /*Video JS Css*/
+        .vjs-default-skin .vjs-big-play-button {
+            left: 50%;
+            margin-top: -22px;
+            margin-left: -22px;
+            top: 50%;
+            font-size: 2em;
+            width: 2em;
+            height: 2em;
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            border-radius: 50%;
+        }
+
+        .vjs-default-skin .vjs-big-play-button:before {
+            line-height: 1.5em;
+        }
+
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function ($) {
+            if ({{$modal}}==1
+            )
+            {
+                $("*").css('opacity', '0');
+                $("html").css('background', 'black');
+                $("html").css('opacity', '1');
+            }
+
+            if ({{$videodelay}}!=
+            0
+            )
+            {
+                setTimeout(function () {
+                    $("video")[0].play();
+                }, {{$videodelay}});
+            }
+        });
+
+        function startVideo() {
+            var o = videojs('example_video_1');
+            if (o.paused()) {
+                o.play();
+            }
+            else {
+                o.pause();
+            }
+        }
+    </script>
 </head>
 <body>
 <?php $posterAttr = empty($vPosterImageFile) ? '' : 'poster="' . $vPosterImageFile . '"'; ?>
-	<video id="example_video_1" onclick="startVideo()" class="video-js vjs-default-skin" {{($videoinit=='onload' ? 'autoplay' : '')}} {{($showcontrols==1 ? 'controls' : '')}} {{($restartwhenfinished==1 ? 'loop' : '')}} {{($mute==1 ? 'mute' : '')}} preload="auto" width="{{$w}}" height="{{$h}}"
-           {{$posterAttr}}
-	      data-setup="{}" style="position:fixed; height:100% !important;">
-	    <source src="{{ ($option==1 ? $vFile : $url) }}" type='video/mp4' />
-	</video>
-	<script type="text/javascript">
-			$(document).ready(function($)
-			{
-				$('#example_video_1').css('width','100%');
-				$('#example_video_1').css('height','100%');
-					setTimeout(function() {
-					    $('#example_video_1').css('width','100%');
-						$('#example_video_1').css('height','100%');
-						$( "*" ).delay(200).animate({opacity: 1}, 300);
-					}, 150);
-			});
-	</script>
+<video id="example_video_1" onclick="startVideo()" class="video-js vjs-default-skin"
+       {{($videoinit=='onload' ? 'autoplay' : '')}} {{($showcontrols==1 ? 'controls' : '')}} {{($restartwhenfinished==1 ? 'loop' : '')}} {{($mute==1 ? 'mute' : '')}} preload="auto"
+       width="{{$w}}" height="{{$h}}"
+       {{$posterAttr}}
+       data-setup="{}" style="position:fixed; height:100% !important;">
+    <source src="{{ ($option==1 ? $vFile : $url) }}" type='video/mp4'/>
+</video>
+<script type="text/javascript">
+    $(document).ready(function ($) {
+        $('#example_video_1').css('width', '100%');
+        $('#example_video_1').css('height', '100%');
+        setTimeout(function () {
+            $('#example_video_1').css('width', '100%');
+            $('#example_video_1').css('height', '100%');
+            $("*").delay(200).animate({opacity: 1}, 300);
+        }, 150);
+    });
+</script>
 </body>
 </html>
 
