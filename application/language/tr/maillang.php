f6547b19ce76217753f3d0eda4ac06e78c01cbd1
Cyclops0571 <srdsaygili@gmail.com>
2016-03-10 17:48:13 +0200

    Demo Üyeliğe Gönderilen Aktivasyon Mailine Dil Desteği

---
+++ application/language/tr/maillang.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'tryitactivationmail' => "Merhaba :USERNAME&nbsp;:SURNAME,</br></br>"
+        . "Galepress ile mobil dünyaya ulaşmak için ilk adımı tamamladın."
+        . "Şimdi üyeliğini aşağıdaki butona tıklayıp doğrulayarak 1 aylık deneme sürümünü kullanmaya başlayabilirsin."
+        . "Önümüzdeki bir ay boyunca Galepress’i en iyi şekilde kullanabilmen için sana çeşitli maillar"
+        . " koçluk edeceğiz ve böylece mümkün olan en kolay şekilde kendi mobil uygulamanı oluşturabileceksin. </br></br>"
+        . "Görüşmek üzere!",
+);
---
+++ application/language/usa/maillang.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'tryitactivationmail' => "Hello  :USERNAME,</br></br>"
+        . "You have completed the first step to conquer the mobile world with Galepress! "
+        . "You can now start using the 1-month trial membership!</br>"
+        . "Click the button below to verify your registration. In the coming months, "
+        . "Galepress will send you various emails to coach you through the application to "
+        . "get the best experience creating your own mobile application!</br></br>"
+        . "If you have any questions, contact us at usa@galepress.com",
+);
