292a2464abae7b3bece5430676705a06f7271876
Cyclops0571 <srdsaygili@gmail.com>
2016-07-27 14:54:22 +0300

    tawk.to

--- application/views/clients/registered.blade.php
+++ application/views/clients/registered.blade.php
@@ -1,10 +1,17 @@
 @layout('layouts.genericmaster')
 
-@section('content') 
-<div class="header">
-    <h2><?php echo __('common.detailpage_caption'); ?> </h2>
-</div>
-<div class="content controls">
-    <h2>{{__('common.registration_succesful')}}</h2>
-</div>
+@section('content')
+    <style type="text/css">
+        .whiteMessage {
+            color: #fff;
+            text-align: center;
+        }
+    </style>
+
+    <div class="header">
+        <h2><?php echo __('common.detailpage_caption'); ?> </h2>
+    </div>
+    <div class="content controls whiteMessage">
+        <h2>{{__('common.registration_succesful')}}</h2>
+    </div>
 @endsection
