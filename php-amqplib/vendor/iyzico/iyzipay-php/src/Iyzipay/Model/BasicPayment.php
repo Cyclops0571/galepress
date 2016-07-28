477f8c062a7ec83c91c72d7a909a4fd96ce730de
Cyclops0571 <srdsaygili@gmail.com>
2016-07-28 11:56:44 +0300

    iyzico kutuphanesi eklendi, idealar silinmek uzere eklendi.

---
+++ php-amqplib/vendor/iyzico/iyzipay-php/src/Iyzipay/Model/BasicPayment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Iyzipay\Model;
+
+use Iyzipay\Model\Mapper\BasicPaymentMapper;
+use Iyzipay\Options;
+use Iyzipay\Request\CreateBasicPaymentRequest;
+
+class BasicPayment extends BasicPaymentResource
+{
+    public static function create(CreateBasicPaymentRequest $request, Options $options)
+    {
+        $rawResult = parent::httpClient()->post($options->getBaseUrl() . "/payment/auth/basic", parent::getHttpHeaders($request, $options), $request->toJsonString());
+        var_dump($rawResult); exit;
+        return BasicPaymentMapper::create($rawResult)->jsonDecode()->mapBasicPayment(new BasicPayment());
+    }
+}
